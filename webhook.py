e5581c36c37e941986725b5f9fda1f79ba52db3c
kamranaxmadov96-creator <kamranaxmadov96@gmail.com>
2025-11-29 03:43:53 +0200

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -6,32 +6,35 @@ from aiogram.fsm.storage.memory import MemoryStorage
 
 TOKEN = os.getenv("BOT_TOKEN")
 WEBHOOK_PATH = "/webhook"
-WEBHOOK_URL = os.getenv("WEBHOOK_URL") + WEBHOOK_PATH
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(storage=MemoryStorage())
+
 
 @dp.message(Command("start"))
 async def start_cmd(message: types.Message):
     await message.answer("Бот работает через webhook! 🚀")
 
-async def webhook_handler(request: web.Request):
-    data = await request.json()
-    update = types.Update(**data)
-    await dp.feed_update(bot, update)
-    return web.Response()
 
-async def on_startup(app: web.Application):
-    await bot.set_webhook(WEBHOOK_URL)
+async def webhook_handler(request: web.Request):
+    try:
+        data = await request.json()
+        update = types.Update(**data)
+        await dp.feed_update(bot, update)
+    except Exception as e:
+        print("WEBHOOK ERROR:", e)
+
+    return web.Response(text="OK", status=200)
+
 
 def create_app():
     app = web.Application()
     app.router.add_post(WEBHOOK_PATH, webhook_handler)
-    app.on_startup.append(on_startup)
     return app
+
 
 app = create_app()
 
-# 🟩 ЭТОТ СТРОКА ОБЯЗАТЕЛЬНА ДЛЯ RENDER!
+
 if __name__ == "__main__":
     web.run_app(app, host="0.0.0.0", port=int(os.getenv("PORT", 10000)))
